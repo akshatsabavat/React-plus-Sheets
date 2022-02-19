6ed9096830d9e8b4a02f2b3751295282d0501bb5
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-02-19 23:00:22 +0530

    feat(FORM) - form JSX added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,8 +3,27 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <div className="TempHolder">
-      <p className="TempHolder__Title">Temporary title tester</p>
+    <div className="TempHolderPage">
+      <p className="TempHolderPage__Title">Sheets Register form</p>
+      <form>
+        <div className="TempHolderPage__inputFeild">
+          <label>Email ID</label>
+          <input type="text" placeholder="@xyz.gmail.com" name="Email ID" />
+        </div>
+        <div className="TempHolderPage__inputFeild">
+          <label>Create password</label>
+          <input type="password" placeholder="********" name="Enter password" />
+        </div>
+        <div className="TempHolderPage__inputFeild">
+          <label>Confirm password</label>
+          <input
+            type="password"
+            placeholder="********"
+            name="Cpnfirm password"
+          />
+        </div>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
