b4eb83a5c7c3ba6ed81ac96f7d7071c10f14345c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-02-19 17:38:40 +0530

    feat(Project) - Boilerplate clean up

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,21 +3,8 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/views/Home/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TempHolder">
+      <p className="TempHolder__Title">Temporary title tester</p>
     </div>
   );
 }
