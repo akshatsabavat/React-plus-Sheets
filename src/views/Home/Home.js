6863566d0fb6a6dcf9b0368b05023f06c72eb2a3
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-02-21 18:49:55 +0530

    feat(Form) - JSX written

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,25 +1,35 @@
-import logo from "../../logo.svg";
 import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="TempHolderPage">
       <p className="TempHolderPage__Title">Sheets Register form</p>
-      <form>
+      <form onSubmit={handleSubmit(handleSubmit(formSubmitAction))}>
         <div className="TempHolderPage__inputFeild">
           <label>Email ID</label>
-          <input type="text" placeholder="@xyz.gmail.com" name="Email ID" />
+          <input
+            type="text"
+            placeholder="@xyz.gmail.com"
+            name="Email ID"
+            ref={register}
+          />
         </div>
         <div className="TempHolderPage__inputFeild">
           <label>Create password</label>
-          <input type="password" placeholder="********" name="Enter password" />
+          <input
+            type="password"
+            placeholder="********"
+            name="Enter password"
+            ref={register}
+          />
         </div>
         <div className="TempHolderPage__inputFeild">
           <label>Confirm password</label>
           <input
             type="password"
             placeholder="********"
-            name="Cpnfirm password"
+            name="Confirm password"
+            ref={register}
           />
         </div>
         <input type="submit" />
